6bc04b00ae29dcb59f00f11754310ec29c9e3c86
Tobi V <hialmberi95@yahoo.com>
2023-04-27 22:56:30 -0500

    Simplify icon and edit readme

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # DIY RSS Feed Reader
 
-This is a heavily commented, beginner friendly RSS Feed Reader Extension- made for [Webcomic Day 2023](https://webcomicday.com/). A website explaining the project can be found [here](diyrss.neocities.org).
+This is a heavily commented, beginner friendly RSS Feed Reader Extension- made for [Webcomic Day 2023](https://webcomicday.com/). A website explaining the project can be found [here](https://diyrss.neocities.org/).
 
-## What it does
+## About the Extension
 
 Editing the `feeds` array in `background.js` allows you to add webcomic's (or other website's) RSS feeds so you can follow them. Clicking on the extension displays a list of RSS Feeds you're following, and when they last updated with a link to view that update.
 
 The extension has a badge that will appear if an RSS feed you follow updated today.
+
+## Extension Guide
+
+People more familiar with coding can probably skip this section and refer to the comments in the code itself. If you're less familiar with code, feel free to refer to this explanation of each file.
+
+### Structure of Files
+
+img
+
+- icon48.png
+
+background.js
+
+LICENSE
+
+manifest.json
+
+README.md
+
+rss.html
+
+script.js
+
+style.css
+
+### What Each File Does
+
+- The `img` folder has the icon that appears in your browser, the icon itself is called `icon48.png` and comes from [Icons8](https://icons8.com/icon/68819/edit-image) (which is a great resource, if you want a different icon). The icon itself is 48 x 48 pixels. Technically, it's good practice to have multiple sizes of your icon, but since this is just something we're doing for fun, it's only got one size.
+- `background.js` runs in the background and creates a badge if a comic updates today. This is where you'll add the RSS Feed links of comics you want to follow, and also where you can change the color of the badge if you want.
+- `LICENSE` this is a Software License and tells people what they're allowed to do with this code- which is anything! I don't care, do whatever you want with this. If having extra files around will confuse you, delete it.
+- `manifest.json` tells your browser about the extension, and tells the extension how to behave on the browser. There's no comments in this file, you would _only_ need to edit the file if you changed the icon file name from `icon48.png`. (Unless you're doing something crazy with my code, this explanation is for new coders after all.)
+- `README.md` is what you're reading right now. Feel free to delete if you don't need the guide.
+- `rss.html` is the structure of the popup for the extension. There's also not much here for you to edit, unless you want to add a title that will appear at the beginning of your list. There's some comments explaining how to do that in the file. You'll notice it's a pretty empty file- that's because the information from the RSS feed to obtained and added by `script.js`.
+- `script.js` is the JavaScript file that communicates with the RSS feed to populate information in the popup. Unless you want to dramatically change the format of the extension, I would recommend you leave this one alone too.
+- `style.css` is what styles your popup- this is where you get to change the color, the font, the size of the font, et cetera. I've left comments outlining what everything does. Don't be afraid to experiment!
--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Hiveworks comic RSS feeds follow a specific format:
-// Comic Name - Page # or Title
+// Comic Name - Page # or Update Title
 // As a result, the RSS feed for the comic repeats the comic name multiple times
 // This function trims the RSS title for each update so it only displays the Page # or Title.
 function editUpdate(update, comicTitle) {
-  if (update.includes(comicTitle)) {
-    // Checks if update name also includes -, so other RSS Feeds that repeat the comic title won't be edited
-    // if you want, you can add else if statements for RSS Feeds you find that repeat names
-    if (update.includes("-")) {
-      updateArray = update.split(" - ");
-      update = updateArray[1];
-    }
-  }
-  return update;
+	if (update.includes(comicTitle)) {
+		// Checks if update name also includes -, so other RSS Feeds that repeat the comic title won't be edited
+		// if you want, you can add else if statements for RSS Feeds you find that repeat names
+		if (update.includes("-")) {
+			updateArray = update.split(" - ");
+			update = updateArray[1];
+		}
+	}
+	return update;
 }
 
 // This function converts the JavaScript Date object into a date a user can read
 function convertDate(pubDate) {
-  let dateString = new Date(pubDate);
-  // getMonth starts with 0 and goes to 11, so we have to add 1 to get the right month number
-  let month = dateString.getMonth() + 1;
-  let day = dateString.getDate();
-  let year = dateString.getFullYear().toString();
-  // Changes year from 2023 (4 digits) to 23 (2 digits), you can remove if you want the full year
-  year = year.substring(2);
-  return month + "/" + day + "/" + year;
+	let dateString = new Date(pubDate);
+	// getMonth starts with 0 and goes to 11, so we have to add 1 to get the right month number
+	let month = dateString.getMonth() + 1;
+	let day = dateString.getDate();
+	let year = dateString.getFullYear().toString();
+	// Changes year from 2023 (4 digits) to 23 (2 digits), you can remove if you want the full year
+	year = year.substring(2);
+	return month + "/" + day + "/" + year;
 }
 
 // This function communicates with the RSS feeds to get information about the most
@@ -31,87 +31,87 @@ function convertDate(pubDate) {
 // This is similar to getRSSFeed on background.js, but gets more information
 // (gets Comic Name, title of the update, the update link, and the update date)
 async function getRSSFeed() {
-  // this is an empty array, we'll add our webcomic updates here
-  const feedArray = [];
+	// this is an empty array, we'll add our webcomic updates here
+	const feedArray = [];
 
-  // Goes through your array of links and gets information from the RSS Feed
-  for (const f of feeds) {
-    // https://corsproxy.io/ fixes CORS errors
-    await fetch("https://corsproxy.io/?" + f)
-      // connects to the RSS Feed so we can get information from it
-      .then((response) => response.text())
-      .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
-      .then((data) => {
-        // gets the comic's title
-        var comicTitle = data.querySelector("title").innerHTML;
+	// Goes through your array of links and gets information from the RSS Feed
+	for (const f of feeds) {
+		// https://corsproxy.io/ fixes CORS errors
+		await fetch("https://corsproxy.io/?" + f)
+			// connects to the RSS Feed so we can get information from it
+			.then((response) => response.text())
+			.then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+			.then((data) => {
+				// gets the comic's title
+				var comicTitle = data.querySelector("title").innerHTML;
 
-        // gets the comic's most recent update (counting in code starts at 0, not 1)
-        const updateData = data.querySelectorAll("item")[0];
+				// gets the comic's most recent update (counting in code starts at 0, not 1)
+				const updateData = data.querySelectorAll("item")[0];
 
-        // gets the name of the most recent update
-        let updateTitle =
-          updateData.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+				// gets the name of the most recent update
+				let updateTitle =
+					updateData.getElementsByTagName("title")[0].childNodes[0].nodeValue;
 
-        // Checks if update name includes comic title (using the function above)
-        updateTitle = editUpdate(updateTitle, comicTitle);
+				// Checks if update name includes comic title (using the function above)
+				updateTitle = editUpdate(updateTitle, comicTitle);
 
-        // gets the update's link
-        let updateLink = updateData.querySelector("link").innerHTML;
+				// gets the update's link
+				let updateLink = updateData.querySelector("link").innerHTML;
 
-        // adds all that information into our array of updates named feedArray
-        feedArray.push([
-          comicTitle,
-          updateTitle,
-          updateLink,
-          // this it the date of the update
-          updateData.querySelector("pubDate").innerHTML,
-        ]);
-        return feedArray;
-      });
-  }
-  // creates the table you see on the extension
-  createRSSList(feedArray);
+				// adds all that information into our array of updates named feedArray
+				feedArray.push([
+					comicTitle,
+					updateTitle,
+					updateLink,
+					// this it the date of the update
+					updateData.querySelector("pubDate").innerHTML,
+				]);
+				return feedArray;
+			});
+	}
+	// creates the table you see on the extension
+	createRSSList(feedArray);
 }
 
 // Sorts the array of updates and adds them to the table
 function createRSSList(dateArray) {
-  // This sorts dateArray by oldest to newest
-  // The code for inserting into the table inserts items on top of each other, so
-  // the array has to be in "reverse" order for the table to display correctly
-  dateArray.sort(function ([a, b, c, d], [w, x, y, z]) {
-    let date1 = { toDate: d };
-    let date2 = { toDate: z };
-    return (
-      Math.abs(Date.now() - new Date(date2.toDate)) -
-      Math.abs(Date.now() - new Date(date1.toDate))
-    );
-  });
+	// This sorts dateArray by oldest to newest
+	// The code for inserting into the table inserts items on top of each other, so
+	// the array has to be in "reverse" order for the table to display correctly
+	dateArray.sort(function ([a, b, c, d], [w, x, y, z]) {
+		let date1 = { toDate: d };
+		let date2 = { toDate: z };
+		return (
+			Math.abs(Date.now() - new Date(date2.toDate)) -
+			Math.abs(Date.now() - new Date(date1.toDate))
+		);
+	});
 
-  // finds the table in the HTML
-  var table = document.getElementById("feed");
+	// finds the table in the HTML
+	var table = document.getElementById("feed");
 
-  // loops through each item in dateArray and adds it to the table
-  dateArray.forEach((item) => {
-    // since items are added in reverse order, we start with the update link & date
-    var row2 = table.insertRow(0);
-    var cell3 = row2.insertCell(0);
-    var cell2 = row2.insertCell(0);
-    cell2.innerHTML = `<a href="${item[2]}" target="_blank">${item[1]}</a>`;
-    cell3.innerHTML = convertDate(item[3]);
+	// loops through each item in dateArray and adds it to the table
+	dateArray.forEach((item) => {
+		// since items are added in reverse order, we start with the update link & date
+		var row2 = table.insertRow(0);
+		var cell3 = row2.insertCell(0);
+		var cell2 = row2.insertCell(0);
+		cell2.innerHTML = `<a href="${item[2]}" target="_blank">${item[1]}</a>`;
+		cell3.innerHTML = convertDate(item[3]);
 
-    // after that, we add the webcomic name
-    var row1 = table.insertRow(0);
-    var cell1 = row1.insertCell(0);
+		// after that, we add the webcomic name
+		var row1 = table.insertRow(0);
+		var cell1 = row1.insertCell(0);
 
-    // checks if webcomic updated today
-    if (convertDate(item[3]) == convertDate(Date.now())) {
-      // Adds a star if today is an update day
-      cell1.innerHTML = `<span class="update-today">&#9733;</span> ` + item[0];
-    } else {
-      cell1.innerHTML = item[0];
-    }
-    cell1.colSpan = "2";
-  });
+		// checks if webcomic updated today
+		if (convertDate(item[3]) == convertDate(Date.now())) {
+			// Adds a star if today is an update day
+			cell1.innerHTML = `<span class="update-today">&#9733;</span> ` + item[0];
+		} else {
+			cell1.innerHTML = item[0];
+		}
+		cell1.colSpan = "2";
+	});
 }
 
 getRSSFeed();
